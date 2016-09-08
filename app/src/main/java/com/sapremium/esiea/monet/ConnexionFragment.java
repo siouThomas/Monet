5d94679fb1d934674b7589acde31062bfc200728
Thomas <thomas.gorand@gmail.com>
2016-09-08 23:36:25 +0200

    Add Fragment Page connection

---
+++ app/src/main/java/com/sapremium/esiea/monet/ConnexionFragment.java
@@ -0,0 +1,80 @@
+package com.sapremium.esiea.monet;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Thomas on 09/09/2016.
+ */
+public class ConnexionFragment extends Fragment
+{
+
+        //private static final String test;
+
+        private OnFragmentInteractionListener mListener;
+
+
+        public static ConnexionFragment newInstance() {
+            ConnexionFragment fragment = new ConnexionFragment();
+            Bundle args = new Bundle();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public ConnexionFragment() {
+            // Required empty public constructor
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View view = inflater
+                    .inflate(R.layout.connexion_fragment, container, false);
+
+            //TODO VUE ACTION
+
+            return view;
+        }
+
+        public void onButtonPressed(String uri) {
+            if (mListener != null) {
+                mListener.onFragmentInteraction(uri);
+            }
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            try {
+                mListener = (OnFragmentInteractionListener) activity;
+            } catch (ClassCastException e) {
+                throw new ClassCastException(activity.toString()
+                        + " must implement OnFragmentInteractionListener");
+            }
+        }
+
+        @Override
+        public void onDetach() {
+            super.onDetach();
+            mListener = null;
+        }
+
+        public interface OnFragmentInteractionListener {
+            // TODO: Update argument type and name
+            public void onFragmentInteraction(String str);
+        }
+
+}
+
--- app/src/main/java/com/sapremium/esiea/monet/MainActivity.java
+++ app/src/main/java/com/sapremium/esiea/monet/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sapremium.esiea.monet;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
