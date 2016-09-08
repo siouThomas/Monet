8e54dc774a7779c57a743a0fb08a9e51584cb07d
Thomas <thomas.gorand@gmail.com>
2016-09-08 23:47:40 +0200

    Add Fragment Page connection + Res

--- app/src/main/java/com/sapremium/esiea/monet/MainActivity.java
+++ app/src/main/java/com/sapremium/esiea/monet/MainActivity.java
@@ -8,11 +8,17 @@ import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ConnexionFragment fragConnexion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //---Fragments
+        fragConnexion = new ConnexionFragment();
+
+        getFragmentManager().beginTransaction().replace(R.id.fragment_content, fragConnexion).commit();
 
     }
 }
